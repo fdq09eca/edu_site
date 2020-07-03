a18f5cfd46a19e13eafd0a0533391a09f6edcfc9
HONG KONG INSTITUTE OF EDUCATIONAL RESEARCH <hkier@cuhk.edu.hk>
2020-07-03 11:15:31 +0800

    try footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,19 +3,14 @@ import InstagramFeed from './InstagramFeed'
 import './Footer.css'
 
 export default () => (
-  <div>
-    {/* <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
-    </h2>
-    <br /> */}
-    <InstagramFeed count="8" />
-    <footer className="footer">
-      <div className="container taCenter">
-        <span>
-          © Copyright {new Date().getFullYear()} HKIER, CUHK. All rights reserved.  <a href="http://www.cuhk.edu.hk/english/disclaimer.html">Disclaimer</a> | <a href="http://www.cuhk.edu.hk/english/privacy.html">Privacy policy</a>
-        </span>
-      </div>
-    </footer>
-  </div>
+  <footer className="footer">
+    <div className="container taCenter">
+      <span>
+        © Copyright {new Date().getFullYear()} HKIER, CUHK. All rights reserved.{' '}
+        <a href="http://www.cuhk.edu.hk/english/disclaimer.html">Disclaimer</a>{' '}
+        |{' '}
+        <a href="http://www.cuhk.edu.hk/english/privacy.html">Privacy policy</a>
+      </span>
+    </div>
+  </footer>
 )
